57e427637cfd179e6f1d3c82324514dec0737e03
royrishin <iam.rishinroy@gmail.com>
2025-03-29 19:30:02 -0500

    inital commit

--- code/spectra-analysis.py
+++ code/spectra-analysis.py
@@ -5,3 +5,14 @@ import matplotlib.pyplot as plt
 
 file = fits.open('data/ganymede.fits')
 print(file.info())
+
+header = file[0].header
+print(header[:5])
+
+spectra = Spectrum1D.read(file)
+spectra
+
+quantity_support()
+fig, ax = plt.subplots()
+plt.title("1D Spectra Visualization of Ganymede")
+ax.step(spectra.spectral_axis, spectra.flux)
